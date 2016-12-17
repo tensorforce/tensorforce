327df3c0046878c7449371ef5faa80d37fe34f66
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2016-12-17 11:01:27 +0000

    trpo continued construction

--- tensorforce/updater/trpo_updater.py
+++ tensorforce/updater/trpo_updater.py
@@ -17,17 +17,19 @@
 Implements trust region policy optimization with general advantage estimation (TRPO-GAE) as
 introduced by Schulman et al.
 
-
+Largely based on https://github.com/ilyasu123/trpo, with a hopefully more readable
+modularisation.
 
 """
 from tensorforce.config import create_config
 from tensorforce.neural_networks.layers import dense
 from tensorforce.neural_networks.neural_network import NeuralNetwork
 from tensorforce.updater.value_function import ValueFunction
+from tensorforce.util.experiment_util import global_seed
+from tensorforce.util.math_util import get_log_prob_gaussian
 import numpy as np
 import tensorflow as tf
 
-from tensorforce.util.experiment_util import global_seed
 
 
 class TRPOUpdater(ValueFunction):
@@ -62,6 +64,7 @@ class TRPOUpdater(ValueFunction):
         self.hidden_layers = NeuralNetwork(self.config.network_layers, self.state, 'training')
 
         self.saver = tf.train.Saver()
+        self.variables = tf.trainable_variables()
         self.session.run(tf.initialize_all_variables())
 
     def create_outputs(self):
@@ -70,20 +73,42 @@ class TRPOUpdater(ValueFunction):
             self.action_means = dense(self.hidden_layers.get_output(),
                                      {'neurons': self.action_count, 'regularization': self.config.regularizer,
                                       'regularization_param': self.config.regularization_param}, 'action_mu')
+            self.prev_action_means = tf.placeholder(tf.float32, [None, self.action_count])
 
             # Random init for log standard deviations
             log_standard_devs_init = tf.Variable(0.01 * self.random.randn(1, self.action_count))
 
             self.action_log_stds = tf.tile(log_standard_devs_init, tf.pack((tf.shape(self.action_means)[0], 1)))
+            self.prev_action_log_stds = tf.placeholder(tf.float32, [None, self.action_count])
+
 
     def create_training_operations(self):
-        pass
+        """
+        Creates TRPO training operations, i.e. the natural gradient update step
+        based on the KL divergence constraint between new and old policy.
+        :return:
+        """
+        with tf.variable_scope("update"):
+            current_log_prob  = get_log_prob_gaussian(self.action_means, self.action_log_stds, self.actions)
+            prev_log_prob = get_log_prob_gaussian(self.prev_action_means, self.prev_action_log_stds, self.actions)
+
+            prob_ratio = tf.exp(current_log_prob - prev_log_prob)
+
+            surrogate = -tf.reduce_mean(prob_ratio * self.advantage)
+
+
 
 
     def get_action(self, state):
         pass
 
     def update(self, batch):
+        """
+        Compute update for one batch of experiences.
+        :param batch:
+        :return:
+        """
+
         pass
 
     def calculate_advantage(self, batch):
--- tensorforce/util/math_util.py
+++ tensorforce/util/math_util.py
@@ -16,6 +16,47 @@
 """
 Contains various mathematical utility functions used for policy gradient methods.
 """
+import numpy as np
+import tensorflow as tf
 
 
+def get_log_prob_gaussian(action_dist_mean, log_std, actions):
+    """
 
+    :param action_dist_mean: Mean of action distribution
+    :param log_std: Log standard deviations
+    :param actions: Actions for which to compute log probabilities.
+    :return:
+    """
+    probability = -tf.square(actions - action_dist_mean) / (2 * tf.exp(2 * log_std)) \
+                  - 0.5 * tf.log(tf.constant(2 * np.pi)) - log_std
+
+    # Sum logs
+    return tf.reduce_sum(probability, 1)
+
+
+def get_kl_divergence_gaussian(mean_a, log_std_a, mean_b, log_std_b):
+    """
+    Kullback-Leibler divergence between Gaussians a and b.
+
+    :param mean_a: Mean of first Gaussian
+    :param log_std_a: Log std of first Gaussian
+    :param mean_b: Mean of second Gaussian
+    :param log_std_b: Log std of second Gaussian
+    :return:
+    """
+    exp_std_a = tf.exp(2 * log_std_a)
+    exp_std_b = tf.exp(2 * log_std_b)
+
+    return tf.reduce_sum(log_std_b - log_std_a
+                         + (exp_std_a + tf.square(mean_a - mean_b)) / (2 * exp_std_b) - 0.5)
+
+
+def get_entropy_gaussian(log_std):
+    """
+    Gaussian entropy, does not depend on mean.
+
+    :param log_std:
+    :return:
+    """
+    return tf.reduce_sum(log_std + tf.constant(0.5 * np.log(2 * np.pi * np.e), tf.float32))

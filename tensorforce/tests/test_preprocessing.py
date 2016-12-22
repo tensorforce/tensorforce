b5038ec20fd144a0f9030c3e2f2fb2c0064d0ce7
Kai Fricke <k@ifricke.com>
2016-12-22 15:31:00 +0100

    Added dependencies for scipy and nose

--- tensorforce/tests/test_preprocessing.py
+++ tensorforce/tests/test_preprocessing.py
@@ -13,25 +13,19 @@
 # limitations under the License.
 # ==============================================================================
 
+"""
+Comment
+"""
+
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-from setuptools import setup
+import unittest
 
-setup(name='tensorforce',
-      version='0.1',
-      description='Reinforcement learning for TensorFlow',
-      url='http://github.com/reinforceio/tensorforce',
-      author='reinforce.io',
-      author_email='contact@reinforce.io',
-      license='Apache 2.0',
-      packages=['tensorforce'],
-      install_requires=[
-          'tensorflow',
-          'numpy',
-          'six',
-          'scipy',
-          'nose'
-      ],
-      zip_safe=False)
+
+def test_preprocessing_grayscale():
+    """
+    Test grayscale preprocessor. Assert shape and values.
+    :return:
+    """

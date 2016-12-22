f73f93f24c605c74509ed49da5a9fe182489695c
Kai Fricke <k@ifricke.com>
2016-12-22 16:13:07 +0100

    Extended concat unittest

--- tensorforce/tests/test_preprocessing.py
+++ tensorforce/tests/test_preprocessing.py
@@ -23,6 +23,8 @@ from __future__ import division
 
 import numpy as np
 
+from six.moves import xrange
+
 from tensorforce import preprocessing
 
 def test_preprocessing_grayscale():
@@ -31,15 +33,15 @@ def test_preprocessing_grayscale():
     """
     pp = preprocessing.grayscale.Grayscale()
 
-    shape = list(np.random.randint(0, 200, size=2)) + [3]
+    shape = list(np.random.randint(0, 20, size=2)) + [3]
     state = np.random.randint(0, 255, size=shape)
 
+    # verify expected shape
     processed_shape = pp.shape(shape)
-
     assert tuple(processed_shape) == tuple(shape[0:2])
 
+    # verify calculated shape
     processed_state = pp.process(state)
-
     assert processed_state.shape == tuple(processed_shape)
 
 
@@ -47,17 +49,26 @@ def test_preprocessing_concat():
     """
     Testing concat preprocessor. Verifies expected and calculated state shapes.
     """
-    concat_length = np.random.randint(1, 100)
+    concat_length = np.random.randint(1, 10)
 
     pp = preprocessing.concat.Concat(concat_length)
 
-    shape = list(np.random.randint(0, 200, size=3))
+    shape = list(np.random.randint(0, 20, size=3))
     state = np.random.randint(0, 255, size=shape)
 
+    # verify expected shape
     processed_shape = pp.shape(shape)
-
     assert tuple(processed_shape) == tuple([concat_length] + shape)
 
+    # verify calculated shape
     processed_state = pp.process(state)
-
     assert processed_state.shape == tuple(processed_shape)
+
+    # verify calculated content
+    states = [state]
+    for i in xrange(concat_length-1):
+        new_state = np.random.randint(0, 255, size=shape)
+        states.append(new_state)
+        processed_state = pp.process(new_state)
+
+    assert (np.array(states) - processed_state).any() == False

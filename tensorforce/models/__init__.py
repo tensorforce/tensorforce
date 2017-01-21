c04c9dea0e3995fce3351c9f5db58a9f006a9505
Kai Fricke <k@ifricke.com>
2017-01-21 11:59:57 +0100

    Fixed some import errors

--- tensorforce/models/__init__.py
+++ tensorforce/models/__init__.py
@@ -13,10 +13,11 @@
 # limitations under the License.
 # ==============================================================================
 
-from tensorforce.models.dqn_model import DQNModel
 from tensorforce.models.model import Model
+from tensorforce.models.dqn_model import DQNModel
 from tensorforce.models.naf_model import NAFModel
+from tensorforce.models.baselines import LinearValueFunction
 from tensorforce.models.trpo_model import TRPOModel
 
 
-__all__ = ['Model', 'DQNModel', 'NAFModel', 'TRPOModel', 'VPGModel']
+__all__ = ['Model', 'DQNModel', 'NAFModel', 'LinearValueFunction', 'TRPOModel', 'VPGModel']

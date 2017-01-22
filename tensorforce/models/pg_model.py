fixed shape errors, now numerical instability in categorical trpo update

tensorforce/models/pg_model.py
@@ -48,7 +48,7 @@ class PGModel(Model):
         self.episode = 0
         self.input_feed = None
 
-        self.advantage = tf.placeholder(tf.float32, shape=[None])
+        self.advantage = tf.placeholder(tf.float32, shape=[None, 1], name='advantage')
         self.policy = None
 
         scope = '' if self.config.tf_scope is None else self.config.tf_scope + '-'
@@ -57,7 +57,7 @@ class PGModel(Model):
 
         self.saver = tf.train.Saver()
         self.actions = tf.placeholder(tf.float32, [None, self.action_count], name='actions')
-        self.prev_action_means = tf.placeholder(tf.float32, [None, self.action_count])
+        self.prev_action_means = tf.placeholder(tf.float32, [None, self.action_count], name='prev_actions')
 
         # From an API perspective, continuous vs discrete might be easier than
         # requiring to set the concrete policy, at least currently
@@ -110,6 +110,7 @@ class PGModel(Model):
         """
         if self.continuous:
             action_log_stds = np.concatenate([path['action_log_stds'] for path in batch])
+            action_log_stds = np.expand_dims(action_log_stds, axis=1)
         else:
             action_log_stds = None
 
@@ -117,6 +118,7 @@ class PGModel(Model):
         actions = np.concatenate([path['actions'] for path in batch])
         batch_advantage = np.concatenate([path["advantage"] for path in batch])
         batch_advantage = zero_mean_unit_variance(batch_advantage)
+        batch_advantage = np.expand_dims(batch_advantage, axis=1)
         states = np.concatenate([path['states'] for path in batch])
 
         return action_log_stds, action_means, actions, batch_advantage, states

tensorforce/models/policies/categorical.py
@@ -5,24 +5,27 @@ import tensorflow as tf
 
 class Categorical(Distribution):
 
+    def __init__(self):
+        super(Categorical, self).__init__()
+
     def kl_divergence(self, dist_a, dist_b):
         prob_a = dist_a['policy_output']
         prob_b = dist_b['policy_output']
 
-        return tf.reduce_sum(prob_a * tf.log(prob_a/prob_b))
+        # Need to ensure numerical stability
+        return tf.reduce_sum(prob_a * tf.log((prob_a + self.epsilon) /(prob_b + self.epsilon)))
 
     def entropy(self, dist):
         prob = dist['policy_output']
 
-        return -tf.reduce_sum(prob * tf.log(prob))
+        return -tf.reduce_sum(prob * tf.log((prob + self.epsilon)))
 
     def log_prob(self, dist, actions):
         prob = dist['policy_output']
 
-        mul = tf.mul(prob, actions)
-        return tf.log(tf.reduce_sum(mul, reduction_indices=[1]))
+        return tf.log(tf.reduce_sum(tf.mul(prob, actions), reduction_indices=[0]))
 
-    def fixed_kl(self, dist, epsilon=1e-6):
+    def fixed_kl(self, dist):
         """
         KL divergence with first param fixed. Used in TRPO update.
 
@@ -30,10 +33,10 @@ class Categorical(Distribution):
         prob = dist['policy_output']
 
         return tf.reduce_sum(tf.stop_gradient(prob)
-                             * tf.log(tf.stop_gradient(prob + epsilon) / (prob + epsilon)))
+                             * tf.log(tf.stop_gradient(prob + self.epsilon) / (prob + self.epsilon)))
 
     def sample(self, dist):
         prob = dist['policy_output']
         cumulative_prob = np.cumsum(prob, axis=0)
 
-        return np.argmax(cumulative_prob > np.random.rand(prob.shape[0], 1), axis=0)
+        return np.argmax(cumulative_prob > np.random.rand(len(dist), 1), axis=0)

tensorforce/models/policies/categorical_one_hot_policy.py
@@ -27,17 +27,21 @@ class CategoricalOneHotPolicy(StochasticPolicy):
 
     def sample(self, state, sample=True):
         output_dist = self.session.run(self.outputs, {self.state: [state]})
+        output_dist = output_dist.ravel()
 
         if sample:
-            action = self.dist.sample(dict(policy_output=output_dist))
+            action = self.dist.sample(dict(policy_output=output_dist))[0]
         else:
             action = int(np.argmax(output_dist))
 
-        #print('action after dist sample ' + str(action))
+        # print('action after dist sample ' + str(action))
 
         one_hot = np.zeros_like(output_dist)
         one_hot[action] = 1
-        return one_hot.ravel(), dict(policy_output=output_dist)
+        # print('action after one hot  ' + str(one_hot))
+
+        # We return a one hot vector and then extract the concrete action in the pg agent
+        return one_hot, dict(policy_output=output_dist)
 
     def get_policy_variables(self):
         return dict(policy_output=self.outputs)

tensorforce/models/policies/gaussian_policy.py
@@ -36,8 +36,9 @@ class GaussianPolicy(StochasticPolicy):
 
         action = action_means + np.exp(action_log_stds) * self.random.randn(*action_log_stds.shape)
 
-        return action.ravel(), dict(policy_output=action_means,
-                                    policy_log_std=action_log_stds)
+        # ravel from [[]] to []
+        return action.ravel(), dict(policy_output=action_means.ravel(),
+                                    policy_log_std=action_log_stds.ravel())
 
     def get_distribution(self):
         return self.dist

tensorforce/models/trpo_model.py
@@ -127,7 +127,10 @@ class TRPOModel(PGModel):
 
         previous_theta = self.flat_variable_helper.get()
 
-        print(action_means.shape)
+        # print('action shape' + str(actions.shape))
+        # print('action means shape' + str(action_means.shape))
+        # print('advantage shape' + str(batch_advantage.shape))
+        # print('log stds shape' + str(action_log_stds.shape))
 
         gradient = self.session.run(self.policy_gradient, self.input_feed)
         zero = np.zeros_like(gradient)

tensorforce/rl_agents/pg_agent.py
@@ -61,6 +61,7 @@ class PGAgent(RLAgent):
         # Cache last action in case action is used multiple times in environment
         self.last_action_means = outputs['policy_output']
         self.last_action = action
+
         #print('action one hot =' + str(action))
 
         if self.continuous:
@@ -109,7 +110,6 @@ class PGAgent(RLAgent):
             if not terminal:
                 self.current_episode['terminated'] = False
                 path = self.get_path()
-
                 self.current_batch.append(path)
 
             print('last means=' + str(self.last_action_means))
@@ -135,7 +135,7 @@ class PGAgent(RLAgent):
         path = {'states': np.concatenate(np.expand_dims(self.current_episode['states'], 0)),
                 'actions': np.array(self.current_episode['actions']),
                 'terminated': self.current_episode['terminated'],
-                'action_means': np.concatenate(self.current_episode['action_means']),
+                'action_means': np.array(self.current_episode['action_means']),
                 'rewards': np.array(self.current_episode['rewards'])}
 
         if self.continuous:
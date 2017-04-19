fixed log std shape in Gaussian policy and KL-divergence axis"

tensorforce/models/pg_model.py
@@ -73,7 +73,7 @@ class PGModel(Model):
         # requiring to set the concrete policy, at least currently
         if self.continuous:
             self.policy = GaussianPolicy(self.network, self.session, self.state, self.random, self.action_count, 'gaussian_policy')
-            self.prev_action_log_stds = tf.placeholder(tf.float32, (None, self.batch_size, self.action_count))
+            self.prev_action_log_stds = tf.placeholder(tf.float32, (None, None, self.action_count))
             self.prev_dist = dict(
                 policy_output=self.prev_action_means,
                 policy_log_std=self.prev_action_log_stds)
@@ -88,7 +88,7 @@ class PGModel(Model):
         size = 1
         for dims in self.state_shape:
             size *= dims
-        self.baseline_value_function = MLPValueFunction(self.session, state_size=size, layer_size=100)  # LinearValueFunction()
+        self.baseline_value_function = LinearValueFunction()
        # self.saver = tf.train.Saver()
 
     def get_action(self, state, episode=1):

tensorforce/models/policies/categorical.py
@@ -23,16 +23,17 @@ class Categorical(Distribution):
         super(Categorical, self).__init__(random)
 
     def kl_divergence(self, dist_a, dist_b):
-        prob_a = dist_a['policy_output']
-        prob_b = dist_b['policy_output']
+        # Why do we reshape here? To go from per episode separate KL divergence to per batch KL divergence
+        prob_a = tf.reshape(dist_a['policy_output'], [-1])
+        prob_b = tf.reshape(dist_b['policy_output'], [-1])
 
         # Need to ensure numerical stability
-        return tf.reduce_sum(prob_a * tf.log((prob_a + self.epsilon) / (prob_b + self.epsilon)), axis=[2])
+        return tf.reduce_sum(prob_a * tf.log((prob_a + self.epsilon) / (prob_b + self.epsilon)), axis=[0])
 
     def entropy(self, dist):
-        prob = dist['policy_output']
+        prob = tf.reshape(dist['policy_output'], [-1])
 
-        return -tf.reduce_sum(prob * tf.log((prob + self.epsilon)))
+        return -tf.reduce_sum(prob * tf.log(prob + self.epsilon), axis=[0])
 
     def log_prob(self, dist, actions):
         prob = dist['policy_output']

tensorforce/models/policies/gaussian.py
@@ -36,25 +36,25 @@ class Gaussian(Distribution):
                       - 0.5 * tf.log(tf.constant(2 * np.pi)) - log_std
 
         # Sum logs
-        return tf.reduce_sum(probability)
+        return tf.reduce_sum(probability, axis=[2])
 
     def kl_divergence(self, dist_a, dist_b,):
-        mean_a = dist_a['policy_output']
-        log_std_a = dist_a['policy_log_std']
+        mean_a = tf.reshape(dist_a['policy_output'], [-1])
+        log_std_a = tf.reshape(dist_a['policy_log_std'], [-1])
 
-        mean_b = dist_b['policy_output']
-        log_std_b = dist_b['policy_log_std']
+        mean_b = tf.reshape(dist_b['policy_output'], [-1])
+        log_std_b = tf.reshape(dist_b['policy_log_std'], [-1])
 
         exp_std_a = tf.exp(2 * log_std_a)
         exp_std_b = tf.exp(2 * log_std_b)
 
         return tf.reduce_sum(log_std_b - log_std_a
-                             + (exp_std_a + tf.square(mean_a - mean_b)) / (2 * exp_std_b) - 0.5)
+                             + (exp_std_a + tf.square(mean_a - mean_b)) / (2 * exp_std_b) - 0.5, axis=[0])
 
     def entropy(self, dist):
-        log_std = dist['policy_log_std']
+        log_std = tf.reshape(dist['policy_log_std'], [-1])
 
-        return tf.reduce_sum(log_std + tf.constant(0.5 * np.log(2 * np.pi * np.e), tf.float32))
+        return tf.reduce_sum(log_std + tf.constant(0.5 * np.log(2 * np.pi * np.e), tf.float32), axis=[0])
 
     def fixed_kl(self, dist):
         """

tensorforce/models/policies/gaussian_policy.py
@@ -34,8 +34,8 @@ class GaussianPolicy(StochasticPolicy):
         with tf.variable_scope(scope):
             action_means = linear(network.output, {'num_outputs': action_count}, 'action_mu')
             # Random init for log standard deviations
-            log_standard_devs_init = tf.Variable(0.01 * random.randn(1, action_count), dtype=tf.float32)
-            action_log_stds = tf.tile(log_standard_devs_init, tf.stack((tf.shape(action_means)[0], 1)))
+            log_standard_devs_init = tf.Variable(0.01 * random.randn(1, 1, action_count), dtype=tf.float32)
+            action_log_stds = tf.tile(log_standard_devs_init, (tf.shape(action_means)[0], tf.shape(action_means)[1], 1))
 
         super(GaussianPolicy, self).__init__(network, [action_means, action_log_stds], session, state, random, action_count)
         self.dist = Gaussian(random)
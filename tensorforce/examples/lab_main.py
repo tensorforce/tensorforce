9bb7a8811fdab92c882e340e6f650598c985e23d
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2017-04-22 15:42:49 +0100

    set default configs for lab

--- tensorforce/examples/lab_main.py
+++ tensorforce/examples/lab_main.py
@@ -40,8 +40,10 @@ def main():
 
     parser.add_argument('-id', '--level-id', default='tests/demo_map',help="DeepMind Lab level id")
     parser.add_argument('-a', '--agent', default='DQNAgent')
-    parser.add_argument('-c', '--agent-config', help="Agent configuration file")
-    parser.add_argument('-n', '--network-config', help="Network configuration file")
+    parser.add_argument('-c', '--agent-config', help="Agent configuration file",
+                        default='examples/configs/dqn_agent.json')
+    parser.add_argument('-n', '--network-config', help="Network configuration file",
+                        default='examples/configs/dqn_network.json')
     parser.add_argument('-e', '--episodes', type=int, default=50000, help="Number of episodes")
     parser.add_argument('-t', '--max-timesteps', type=int, default=2000, help="Maximum number of timesteps per episode")
     parser.add_argument('-m', '--monitor', help="Save results to this directory")

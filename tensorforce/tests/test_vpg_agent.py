4c2dcf603e6095cfaf88d16687736a2757c33f98
Kai Fricke <k@ifricke.com>
2017-05-12 13:25:29 +0100

    added tests for continuous action spaces (trpo&vpg)

--- tensorforce/tests/test_vpg_agent.py
+++ tensorforce/tests/test_vpg_agent.py
@@ -63,4 +63,44 @@ class TestVPGAgent(unittest.TestCase):
             if sum(rewards) == 100.0:
                 return
 
+        print("Did not reach goal of 100 consecutive correct actions (got {})".format(sum(rewards)))
+        self.assertTrue(False)
+
+
+    def test_vpg_agent_continuous(self):
+
+        config = {
+            'seed': 12,
+            'batch_size': 8,
+            'max_episode_length': 4,
+            'continuous': True,
+            'state_shape': (2,),
+            'actions': 1
+        }
+
+        tf.reset_default_graph()
+
+        config = create_config(config)
+        network_builder = NeuralNetwork.layered_network(layers=[{'type': 'dense', 'num_outputs': 16}])
+
+        agent = VPGAgent(config=config, network_builder=network_builder)
+
+        state = (1, 0)
+        rewards = [0.0] * 100
+        for n in xrange(50000):
+            action = agent.get_action(state=state)
+            if action >= -1.0 and action <= 1.0:
+                state = (1, 0)
+                reward = 0.0
+                terminal = False
+            else:
+                state = (0, 1)
+                reward = 1.0
+                terminal = True
+            agent.add_observation(state=state, action=action, reward=reward, terminal=terminal)
+            rewards[n % 100] = reward
+            if sum(rewards) == 100.0:
+                return
+
+        print("Did not reach goal of 100 consecutive correct actions (got {})".format(sum(rewards)))
         self.assertTrue(False)

152869662aa95a53a464ec0e90819b5085125c97
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2017-07-01 11:12:37 +0100

    Cmd args in bazel build file.

--- BUILD
+++ BUILD
@@ -1,5 +1,10 @@
 package(default_visibility = ["//visibility:public"])
 
+env_args = [
+  "--agent-config examples/configs/dqn_network.json",
+  "--network-config examples/configs/dqn_network.json"
+]
+
 py_library(
     name = "tensorforce",
     imports = [":tensorforce"],
--- examples/lab_main.py
+++ examples/lab_main.py
@@ -46,9 +46,9 @@ def main():
     parser.add_argument('-id', '--level-id', default='tests/demo_map',help="DeepMind Lab level id")
     parser.add_argument('-a', '--agent', default='DQNAgent')
     parser.add_argument('-c', '--agent-config', help="Agent configuration file",
-                        default='/configs/dqn_agent.json')
+                        default='examples/configs/dqn_agent.json')
     parser.add_argument('-n', '--network-config', help="Network configuration file",
-                        default='/configs/dqn_network.json')
+                        default='examples/configs/dqn_network.json')
     parser.add_argument('-e', '--episodes', type=int, default=1000, help="Number of episodes")
     parser.add_argument('-t', '--max-timesteps', type=int, default=200, help="Maximum number of timesteps per episode")
     parser.add_argument('-m', '--monitor', help="Save results to this directory")

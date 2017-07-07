762304bb46577275d879ccd19d066515be9bdd09
Kai Fricke <k@ifricke.com>
2017-07-07 18:05:21 +0200

    added trpo docs

--- tensorforce/agents/trpo_agent.py
+++ tensorforce/agents/trpo_agent.py
@@ -26,6 +26,35 @@ from tensorforce.models import TRPOModel
 
 
 class TRPOAgent(BatchAgent):
+    """
+    Trust Region Policy Optimization ([Schulman et al., 2015](https://arxiv.org/abs/1502.05477)) agent.
+
+    ## Configuration
+
+    Each agent requires the following ``Configuration`` parameters:
+
+    * `states`: dict containing one or more state definitions.
+    * `actions`: dict containing one or more action definitions.
+    * `preprocessing`: dict or list containing state preprocessing configuration.
+    * `exploration`: dict containing action exploration configuration.
+
+    The `BatchAgent` class additionally requires the following parameters:
+
+    * `batch_size`: integer of the batch size.
+
+    A Policy Gradient Model expects the following additional configuration parameters:
+
+    * `sample_actions`: boolean of whether to sample actions
+    * `baseline`: string indicating the baseline value function (currently 'linear' or 'mlp')
+    * `baseline_args`: list of arguments for the baseline value function
+    * `baseline_kwargs`: dict of keyword arguments for the baseline value function
+    * `generalized_advantage_estimation`: boolean indicating whether to use GAE estimation
+    * `gae_lambda`: float of the Generalized Advantage Estimation lambda
+    * `normalize_advantage`: boolean indicating whether to normalize the advantage or not
+
+
+    """
+
 
     name = 'TRPOAgent'
     model = TRPOModel

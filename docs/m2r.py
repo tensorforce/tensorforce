841b41f5d1460972181bcf42e13da2ffc79c4546
Kai Fricke <k@ifricke.com>
2017-07-13 13:52:15 +0200

    added copyright and license notices to mistune and m2r

--- docs/m2r.py
+++ docs/m2r.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# https://github.com/miyakogi/m2r
+# Licensed under the MIT License
+
 from __future__ import print_function, unicode_literals
 import os
 import re

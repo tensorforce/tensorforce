0a5970d9b876406f517bf5ac9cba754d885f4be8
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2017-07-13 20:23:12 +0100

    Improved quickstart config for TRPO

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -30,34 +30,46 @@ env = OpenAIGym('CartPole-v0')
 
 # Create a Trust Region Policy Optimization agent
 agent = TRPOAgent(config=Configuration(
-  loglevel="info",
-  batch_size=200,
-  override_line_search=False,
-  use_gae=True,
-  normalize_advantage=False,
-  gae_lambda=0.97,
-  cg_iterations=20,
-  cg_damping=0.001,
-  line_search_steps=20,
-  max_kl_divergence=0.01,
-  gamma=0.97,
-  continuous=False,
-  preprocessing=None,
-  states=env.states,
-  actions=env.actions,
-  network=layered_network_builder([dict(type='dense', size=10)])
+    loglevel="info",
+    batch_size=100,
+    baseline="mlp",
+    baseline_args=None,
+    baseline_kwargs=dict(
+        size=100,
+        repeat_update=100
+    ),
+    override_line_search=False,
+    generalized_advantage_estimation=True,
+    normalize_advantage=False,
+    gae_lambda=0.97,
+    cg_iterations=20,
+    cg_damping=0.001,
+    line_search_steps=20,
+    max_kl_divergence=0.005,
+    gamma=0.97,
+    continuous=False,
+    preprocessing=None,
+    states=env.states,
+    actions=env.actions,
+    network=layered_network_builder([dict(type='dense', size=10, activation='selu')])
 ))
 
 # Create the runner
 runner = Runner(agent=agent, environment=env)
 
+
 # Callback function printing episode statistics
 def episode_finished(r):
-    print("Finished episode {ep} after {ts} timesteps (reward: {reward})".format(ep=r.episode, ts=r.timestep, reward=r.episode_rewards[-1]))
+    print("Finished episode {ep} after {ts} timesteps (reward: {reward})".format(ep=r.episode, ts=r.timestep,
+                                                                                 reward=r.episode_rewards[-1]))
     return True
+
 
 # Start learning
 runner.run(episodes=3000, max_timesteps=200, episode_finished=episode_finished)
 
 # Print statistics
-print("Learning finished. Total episodes: {ep}. Average reward of last 100 episodes: {ar}.".format(ep=runner.episode, ar=np.mean(runner.episode_rewards[-100:])))
+print("Learning finished. Total episodes: {ep}. Average reward of last 100 episodes: {ar}.".format(ep=runner.episode,
+                                                                                                   ar=np.mean(
+                                                                                                       runner.episode_rewards[
+                                                                                                       -100:])))

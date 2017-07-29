c25dc1b254de8b92740417976b34943f2763bcd7
Kai Fricke <k@ifricke.com>
2017-07-29 15:17:01 +0200

    removed odd action from dqfd multi test

--- tensorforce/tests/test_dqfd_agent.py
+++ tensorforce/tests/test_dqfd_agent.py
@@ -113,7 +113,7 @@ class TestDQFDAgent(unittest.TestCase):
             for n in xrange(50):
                 if terminal:
                     state = environment.reset()
-                action = dict(action0=1, action1=(1, 1), action2=((1, 1),))
+                action = dict(action0=1, action1=(1, 1))
                 state, reward, terminal = environment.execute(action=action)
                 demonstration = dict(state=state, action=action, reward=reward, terminal=terminal, internal=[])
                 demonstrations.append(demonstration)

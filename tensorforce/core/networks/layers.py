9067c6bdcdc177cad76b6b644a9174c4fda0a88f
Alexander Kuhnle <aok25@cl.cam.ac.uk>
2017-08-29 10:43:06 +0100

    fixed problems with multiple LSTMs, introduced scope argument for layers

--- tensorforce/core/networks/layers.py
+++ tensorforce/core/networks/layers.py
@@ -23,6 +23,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
+from collections import Counter
 import json
 from math import sqrt
 import os
@@ -33,7 +34,7 @@ import tensorflow as tf
 from tensorforce import TensorForceError, util
 
 
-def flatten(x, summary_level=0):
+def flatten(x, scope='flatten', summary_level=0):
     """Flatten layer.
 
     Args:
@@ -42,13 +43,13 @@ def flatten(x, summary_level=0):
     Returns: Input tensor reshaped to 1d tensor
 
     """
-    with tf.variable_scope('flatten'):
+    with tf.variable_scope(scope):
         x = tf.reshape(tensor=x, shape=(-1, util.prod(x.get_shape().as_list()[1:])))
 
     return x
 
 
-def nonlinearity(x, name='relu', summary_level=0):
+def nonlinearity(x, name='relu', scope='nonlinearity', summary_level=0):
     """ Applies a non-linearity to an input and returns the result.
 
     Args:
@@ -59,7 +60,7 @@ def nonlinearity(x, name='relu', summary_level=0):
     Returns:
 
     """
-    with tf.variable_scope('nonlinearity'):
+    with tf.variable_scope(scope):
         if name == 'elu':
             x = tf.nn.elu(features=x)
         elif name == 'relu':
@@ -87,7 +88,7 @@ def nonlinearity(x, name='relu', summary_level=0):
     return x
 
 
-def linear(x, size, weights=None, bias=True, l2_regularization=0.0, summary_level=0):
+def linear(x, size, weights=None, bias=True, l2_regularization=0.0, scope='linear', summary_level=0):
     """
     Linear layer.
 
@@ -108,7 +109,7 @@ def linear(x, size, weights=None, bias=True, l2_regularization=0.0, summary_leve
         raise TensorForceError('Invalid input rank for linear layer: {},'
                                ' must be 2.'.format(input_rank))
 
-    with tf.variable_scope('linear'):
+    with tf.variable_scope(scope):
         shape = (x.shape[1].value, size)
         weights_variable = True
         if weights is None:
@@ -161,7 +162,7 @@ def linear(x, size, weights=None, bias=True, l2_regularization=0.0, summary_leve
     return x
 
 
-def dense(x, size, bias=True, activation='relu', l2_regularization=0.0, summary_level=0):
+def dense(x, size, bias=True, activation='relu', l2_regularization=0.0, scope='dense', summary_level=0):
     """
     Fully connected layer.
 
@@ -180,7 +181,7 @@ def dense(x, size, bias=True, activation='relu', l2_regularization=0.0, summary_
         raise TensorForceError('Invalid input rank for linear layer: {},'
                                ' must be 2.'.format(input_rank))
 
-    with tf.variable_scope('dense'):
+    with tf.variable_scope(scope):
         x = linear(x=x, size=size, bias=bias, l2_regularization=l2_regularization)
         x = nonlinearity(x=x, name=activation, summary_level=summary_level)
 
@@ -190,7 +191,7 @@ def dense(x, size, bias=True, activation='relu', l2_regularization=0.0, summary_
 
 
 def conv2d(x, size, window=3, stride=1, padding='SAME', bias=False, activation='relu',
-           l2_regularization=0.0, summary_level=0):
+           l2_regularization=0.0, scope='conv2d', summary_level=0):
     """A 2d convolutional layer.
 
     Args:
@@ -210,7 +211,7 @@ def conv2d(x, size, window=3, stride=1, padding='SAME', bias=False, activation='
     if input_rank != 4:
         raise TensorForceError('Invalid input rank for conv2d layer: {}, must be 4'.format(input_rank))
 
-    with tf.variable_scope('conv2d'):
+    with tf.variable_scope(scope):
         shape = (window, window, x.shape[3].value, size)
         stddev = min(0.1, sqrt(2.0 / size))
         filters = tf.Variable(initial_value=tf.random_normal(shape=shape, stddev=stddev), name='W')
@@ -234,7 +235,7 @@ def conv2d(x, size, window=3, stride=1, padding='SAME', bias=False, activation='
     return x
 
 
-def lstm(x, size=None, summary_level=0):
+def lstm(x, size=None, scope='lstm', summary_level=0):
     """
 
     Args:
@@ -251,7 +252,7 @@ def lstm(x, size=None, summary_level=0):
     if not size:
         size = x.get_shape()[1].value
 
-    with tf.variable_scope('lstm'):
+    with tf.variable_scope(scope):
         internal_input = tf.placeholder(dtype=tf.float32, shape=(None, 2, size))
         lstm_cell = tf.contrib.rnn.LSTMCell(num_units=size)
         c = internal_input[:, 0, :]
@@ -299,12 +300,15 @@ def layered_network_builder(layers_config):
         internal_outputs = []
         internal_inits = []
 
+        layer_counter = Counter()
         for layer_config in layers_config:
+            scope = layer_config['type'] + str(layer_counter[layer_config['type']])
             x = util.get_object(
                 obj=layer_config,
                 predefined=layers,
-                kwargs=dict(x=x, summary_level=summary_level)
+                kwargs=dict(x=x, scope=scope, summary_level=summary_level)
             )
+            layer_counter[layer_config['type']] += 1
             if isinstance(x, list) or isinstance(x, tuple):
                 assert len(x) == 4
                 internal_inputs.extend(x[1])

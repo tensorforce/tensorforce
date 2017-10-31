bc5ee9d944f56e36a589e4b0cb7b2fd7d811d4cc
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2017-10-31 21:44:17 +0000

    Threaded-ale initial api update.

--- examples/threaded_ale.py
+++ examples/threaded_ale.py
@@ -14,13 +14,14 @@
 # ==============================================================================
 
 """
-Arcade Learning Environment execution
+Arcade Learning Environment execution.
 """
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from six.moves import xrange
 import argparse
 import logging
 import os
@@ -30,7 +31,7 @@ import numpy as np
 
 from tensorforce import Configuration
 from tensorforce.agents import agents as AgentsDictionary
-from tensorforce.core.networks import from_json
+import json
 from tensorforce.execution import ThreadedRunner
 from tensorforce.contrib.ale import ALE
 
@@ -38,8 +39,8 @@ from tensorforce.contrib.ale import ALE
 """
 To replicate the Asynchronous Methods for Deep Reinforcement Learning paper (https://arxiv.org/abs/1602.01783)
 Nstep DQN:
-    python threaded_ale.py <path_to_rom> -a DQNNstepAgent -c ./configs/dqn_nstep_agent_visual.json \
-    -n ./configs/dqn_2013_network_visual.json -fs 4 -rc -1 1 -ea -w 16
+    python threaded_ale.py <path_to_rom> -a dqn_nstep_agent -c ./configs/dqn_nstep_agent_visual.json \
+    -n ./configs/dqn_2013_network_visual.json -fs 4 -ea -w 16
 
     Note: batch_size in the config should be set to n+1 where n is the desired number of steps
 """
@@ -51,7 +52,7 @@ def main():
     parser.add_argument('rom', help="File path of the rom")
     parser.add_argument('-a', '--agent', help='Agent')
     parser.add_argument('-c', '--agent-config', help="Agent configuration file")
-    parser.add_argument('-n', '--network-config', help="Network configuration file")
+    parser.add_argument('-n', '--network-spec', help="Network configuration file")
     parser.add_argument('-w', '--workers', help="Number of threads to run where the model is shared", type=int, default=16)
     parser.add_argument('-fs', '--frame-skip', help="Number of frames to repeat action", type=int, default=1)
     parser.add_argument('-rap', '--repeat-action-probability', help="Repeat action probability", type=float, default=0.0)
@@ -78,33 +79,50 @@ def main():
                         loss_of_life_reward=args.loss_of_life_reward,
                         display_screen=args.display_screen) for t in range(args.workers)]
 
-    if args.network_config:
-        network = from_json(args.network_config)
+
+    if args.network_spec:
+        with open(args.network_spec, 'r') as fp:
+            network_spec = json.load(fp=fp)
     else:
-        network = None
+        network_spec = None
         logger.info("No network configuration provided.")
+
 
     agent_configs = []
     for i in range(args.workers):
         agent_config = Configuration.from_json(args.agent_config)
 
-        # optionally overwrite epsilon final values
-        if "exploration" in agent_config and "epsilon" in agent_config.exploration.type:
+        # Optionally overwrite epsilon final values
+        if "exploration" in agent_config and agent_config.exploration["type"] == "epsilon_anneal":
             # epsilon annealing is based on the global step so divide by the total workers
-            epsilon_timesteps = agent_config.exploration.epsilon_timesteps // args.workers
-            agent_config.exploration.epsilon_timesteps = epsilon_timesteps
+            epsilon_timesteps = agent_config.exploration["epsilon_timesteps"] // args.workers
+            agent_config.exploration["epsilon_timesteps"] = epsilon_timesteps
             if args.epsilon_annealing:
                 # epsilon final values are [0.5, 0.1, 0.01] with probabilities [0.3, 0.4, 0.3]
                 epsilon_final = np.random.choice([0.5, 0.1, 0.01], p=[0.3, 0.4, 0.3])
-                agent_config.exploration.epsilon_final = epsilon_final
+                agent_config.exploration["epsilon_final"] = epsilon_final
 
-        agent_config.default(dict(states=environments[0].states, actions=environments[0].actions, network=network))
+
         agent_configs.append(agent_config)
 
-    # let the first agent create the model
-    agent = AgentsDictionary[args.agent](config=agent_configs[-1])
-    # then create agents with a shared model
-    agents = [AgentsDictionary[args.agent](config=agent_configs[t], model=agent.model) for t in range(args.workers - 1)] + [agent]
+    # Let the first agent create the model
+    agent = AgentsDictionary[args.agent](
+        states_spec=environments[0].states,
+        actions_spec=environments[0].actions,
+        network_spec=network_spec,
+        config=agent_configs[-1]
+    )
+    agents = [agent]
+    # Manually assign model
+    for i in xrange(args.workers - 1):
+        worker = AgentsDictionary[args.agent](
+                    states_spec=environments[0].states,
+                    actions_spec=environments[0].actions,
+                    network_spec=network_spec,
+                    config=agent_configs[-1]
+                )
+        worker.model = agent.model
+        agents.append(worker)
 
     if args.load:
         load_dir = os.path.dirname(args.load)
@@ -145,7 +163,7 @@ def main():
             logger.info("Average of last 100 rewards: {}".format(sum(reward_list[-100:]) / 100))
         logger.info('=' * 40)
 
-    # create runners
+    # Create runners
     threaded_runner = ThreadedRunner(agents, environments, repeat_actions=1,
                                      save_path=args.save, save_episodes=args.save_episodes)
 

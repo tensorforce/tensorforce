0fc5ec2d969ffa81c49552e9132320f84b52fce1
Kai Fricke <k@ifricke.com>
2018-02-18 17:56:43 +0100

    updated ornstein-uhlenbeck process to keep an own state for each action dimension

--- tensorforce/core/explorations/ornstein_uhlenbeck_process.py
+++ tensorforce/core/explorations/ornstein_uhlenbeck_process.py
@@ -43,8 +43,7 @@ class OrnsteinUhlenbeckProcess(Exploration):
         super(OrnsteinUhlenbeckProcess, self).__init__(scope=scope, summary_labels=summary_labels)
 
     def tf_explore(self, episode, timestep, action_shape):
-        normal_sample = tf.random_normal(shape=(), mean=0.0, stddev=1.0)
-        state = tf.get_variable(name='ornstein_uhlenbeck', dtype=util.tf_dtype('float'), initializer=(self.mu,))
-        state = tf.assign_add(ref=state, value=(self.theta * (self.mu - state) + self.sigma * normal_sample))
-
-        return tf.reshape(state, shape=action_shape)
+        normal_sample = tf.random_normal(shape=action_shape.shape, mean=0.0, stddev=1.0)
+        state = tf.get_variable(name='ornstein_uhlenbeck', dtype=util.tf_dtype('float'), shape=action_shape.shape,
+                                initializer=tf.constant_initializer(self.mu))
+        return tf.assign_add(ref=state, value=(self.theta * (self.mu - state) + self.sigma * normal_sample))

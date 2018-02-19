48f55ba96207f92abffe30d2d7fe47dc12cd2c52
sven1977 <svenmika1977@gmail.com>
2018-02-19 11:31:36 +0100

    Add timeout (sec) parameter to RemoteEnvironment.connect().

--- tensorforce/contrib/remote_environment.py
+++ tensorforce/contrib/remote_environment.py
@@ -22,6 +22,7 @@ import errno
 import os
 from tensorforce import TensorForceError
 import logging
+import time
 
 
 class RemoteEnvironment(Environment):
@@ -54,20 +55,34 @@ class RemoteEnvironment(Environment):
         """
         self.disconnect()
 
-    def connect(self):
+    def connect(self, timeout=600):
         """
         Starts the server tcp connection on the given host:port.
+
+        Args:
+            timeout (int): The time (in seconds) for which we will attempt a connection to the remote
+                (every 5sec). After that (or if timeout is None or 0), an error is raised.
         """
         # If we are already connected, return error.
         if self.socket:
             raise TensorForceError("Already connected to {}:{}. Only one connection allowed at a time. " +
                                    "Close first by calling `close`!".format(self.host, self.port))
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.settimeout(5)
-        err = self.socket.connect_ex((self.host, self.port))
+
+        if timeout < 5 or timeout is None:
+            timeout = 5
+
+        err = 0
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            self.socket.settimeout(5)
+            err = self.socket.connect_ex((self.host, self.port))
+            if err == 0:
+                break
+            time.sleep(1)
         if err != 0:
-            raise TensorForceError("Error when trying to connect to {}:{}: errno={} '{}'".
-                                   format(self.host, self.port, errno.errorcode[err], os.strerror(err)))
+            raise TensorForceError("Error when trying to connect to {}:{}: errno={} errcode='{}' '{}'".
+                                   format(self.host, self.port, err, errno.errorcode[err], os.strerror(err)))
 
     def disconnect(self):
         """

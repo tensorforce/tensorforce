ae94902e9412f9fc8022227818346fe02172b030
sven1977 <svenmika1977@gmail.com>
2018-02-27 13:42:27 +0100

    Fix dqn_visual.json update_mode. Fixed bug in model.py:observe(). Need to use raw_session otherwise tf will abort with missing states/actions-placeholder error in case we use states/actions-summaries. Added utility function to utils to get the dependencies for a given tensor.

--- tensorforce/util.py
+++ tensorforce/util.py
@@ -110,6 +110,22 @@ def map_tensors(fn, tensors):
         return fn(tensors)
 
 
+def get_tensor_dependencies(tensor):
+    """
+    Utility method to get all dependencies (including placeholders) of a tensor (backwards through the graph).
+
+    Args:
+        tensor (tf.Tensor): The input tensor.
+
+    Returns: Set of all dependencies (including needed placeholders) for the input tensor.
+    """
+    dependencies = set()
+    dependencies.update(tensor.op.inputs)
+    for sub_op in tensor.op.inputs:
+        dependencies.update(get_tensor_dependencies(sub_op))
+    return dependencies
+
+
 def get_object(obj, predefined_objects=None, default_object=None, kwargs=None):
     """
     Utility method to map some kind of object specification to its content,

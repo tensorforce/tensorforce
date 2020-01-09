fef3b8e1bfcb5250eef265ef7182f87e99233dc6
AlexKuhnle <alexkuhnle@t-online.de>
2020-01-09 22:09:33 +0000

    fix requirement TF version 2.0.0

--- setup.py
+++ setup.py
@@ -91,8 +91,8 @@ setup(
     python_requires='>=3.5',
     install_requires=install_requires,
     extras_require=dict(
-        tf=['tensorflow'],
-        tf_gpu=['tensorflow-gpu'],
+        tf=['tensorflow==2.0.0'],
+        tf_gpu=['tensorflow-gpu==2.0.0'],
         tfa=['tensorflow-addons'],
         docs=['m2r', 'recommonmark', 'sphinx', 'sphinx-rtd-theme'],
         envs=['gym[all]', 'gym-retro', 'mazeexp', 'vizdoom'],

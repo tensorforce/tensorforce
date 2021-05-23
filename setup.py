b92701ba03314c1b5a42b51d83583d92f0b615f0
AlexKuhnle <alexkuhnle@t-online.de>
2021-05-23 14:38:21 +0100

    Update gym and pillow version requirement

--- setup.py
+++ setup.py
@@ -140,11 +140,11 @@ setup(
         tfa=['tensorflow-addons >= 0.13.0'],
         tune=['hpbandster >= 0.7.4'],
         envs=[
-            'ale-py', 'gym[atari,box2d,classic_control] >= 0.18.0', 'box2d >= 2.3.10',
+            'ale-py', 'gym[atari,box2d,classic_control] >= 0.18.3', 'box2d >= 2.3.10',
             'gym-retro >= 0.8.0', 'vizdoom == 1.1.7'
         ],
         ale=['ale-py'],
-        gym=['gym[atari,box2d,classic_control] >= 0.18.0', 'box2d >= 2.3.10'],
+        gym=['gym[atari,box2d,classic_control] >= 0.18.3', 'box2d >= 2.3.10'],
         retro=['gym-retro >= 0.8.0'],
         vizdoom=['vizdoom == 1.1.7'],
         carla=['pygame', 'opencv-python'],
